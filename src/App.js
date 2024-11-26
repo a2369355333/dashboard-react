18016405714005e1ca0f9cd0d2d36e559f1626df
Oreo <a2369355333@yahoo.com.tw>
2024-11-26 22:30:21 +0800

    Update to use HashRouter for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/home";
 import Scoring from "./pages/scoring/scoring";
 import HeadCount from "./pages/headcount/headcount";
@@ -24,3 +24,4 @@ function App() {
 }
 
 export default App;
+
