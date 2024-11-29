61c61f6b9af85bf43eda4abc538dd9fce0c401d1
Oreo <a2369355333@yahoo.com.tw>
2024-11-29 18:28:17 +0800

    RWD

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,18 +1,22 @@
-import { useState, useMemo, useEffect } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { Button } from "@mui/material";
 import Loading from "../../components/loading";
 import { useLoading } from "../../provider/loading-provider";
 import PptxGenJS from "pptxgenjs";
 
 const Home = () => {
-  const { isLoading, setIsLoading, screenshotInfo, setScreenshotInfo } = useLoading();
+  const { isLoading, setIsLoading, screenshotInfo, setScreenshotInfo } =
+    useLoading();
   const navigate = useNavigate();
   const navigateList = ["/scoring", "/ratio", "/headcount", "/"];
 
   const generateReport = () => {
-    if (screenshotInfo.scoring || screenshotInfo.ratio || screenshotInfo.headcount) {
+    if (
+      screenshotInfo.scoring ||
+      screenshotInfo.ratio ||
+      screenshotInfo.headcount
+    ) {
       setScreenshotInfo({ scoring: [], ratio: [], headcount: [] });
     }
     setIsLoading(true);
@@ -31,7 +35,7 @@ const Home = () => {
       const x = index === 0 ? 0 : 0.5;
       const y = index === 0 ? 0.25 : 0.8;
       const w = index === 0 ? 10 : 9;
-    
+
       slide.addImage({ data: image, x: x, y: y, w: w, h: 4 });
     });
     screenshotInfo.ratio.forEach((image) => {
@@ -48,8 +52,12 @@ const Home = () => {
   };
 
   useEffect(() => {
-    if (((screenshotInfo.scoring && screenshotInfo.scoring.length > 0) || (screenshotInfo.ratio && screenshotInfo.ratio.length > 0) || (screenshotInfo.headcount && screenshotInfo.headcount.length > 0)) && isLoading) {
-          createPPT();
+    if (
+      ((screenshotInfo.scoring && screenshotInfo.scoring.length > 0) ||
+        (screenshotInfo.ratio && screenshotInfo.ratio.length > 0) ||
+        (screenshotInfo.headcount && screenshotInfo.headcount.length > 0)) && isLoading
+    ) {
+      createPPT();
     }
   }, [isLoading]);
 
@@ -58,28 +66,26 @@ const Home = () => {
       {isLoading && <Loading />}
       <div className="relative w-[28vw] h-[28vw]">
         <Link to="/scoring">
-          <button className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-64 h-64 bg-red-300 text-white text-2xl rounded-full shadow-xl hover:bg-red-500 hover:shadow-2xl transition duration-500">
-            Scoring
+          <button className="flex items-center justify-center w-[16vw] h-[16vw] absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-red-300 text-white text-2xl rounded-full shadow-xl hover:bg-red-500 hover:shadow-2xl transition duration-500">
+            <span className="text-[2vw]">Scoring</span>
           </button>
         </Link>
         <Link to="/headcount">
-          <button className="absolute bottom-0 left-0 transform -translate-x-1/2 w-64 h-64 bg-green-300 text-white text-2xl rounded-full shadow-xl hover:bg-green-500 hover:shadow-2xl transition duration-500">
-            HeadCount
+          <button className="flex items-center justify-center w-[16vw] h-[16vw] absolute bottom-0 left-0 transform -translate-x-1/2 bg-green-300 text-white text-2xl rounded-full shadow-xl hover:bg-green-500 hover:shadow-2xl transition duration-500">
+            <span className="text-[2vw]">HeadCount</span>
           </button>
         </Link>
         <Link to="/ratio">
-          <button className="absolute bottom-0 right-0 transform translate-x-1/2 w-64 h-64 bg-blue-300 text-white text-2xl rounded-full shadow-xl hover:bg-blue-500 hover:shadow-2xl transition duration-500">
-            Ratio
+          <button className="flex items-center justify-center w-[16vw] h-[16vw] absolute bottom-0 right-0 transform translate-x-1/2 bg-blue-300 text-white text-2xl rounded-full shadow-xl hover:bg-blue-500 hover:shadow-2xl transition duration-500">
+            <span className="text-[2vw]">Ratio</span>
           </button>
         </Link>
-        <Button
-          variant="contained"
-          color="primary"
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[180px] h-[50px] rounded-lg shadow-lg"
+        <button
+          className="flex items-center justify-center w-[10vw] h-[3vw] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-blue-500 text-white text-2xl rounded-lg shadow-xl hover:bg-blue-600"
           onClick={generateReport}
         >
-          Generate Report
-        </Button>
+          <span className="text-[1vw]">Generate Report</span>
+        </button>
       </div>
     </div>
   );
